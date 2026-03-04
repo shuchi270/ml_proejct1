61799bd31a4efd9c5ca132b6c268386b80831da3
shuchi270 <goelshuchi273@gmail.com>
2026-03-04 23:26:13 +0530

    Exploratory Data Analysis

--- src/logger.py
+++ src/logger.py
@@ -15,6 +15,8 @@ logging.basicConfig(
     level= logging.INFO
 )
 
-if __name__ =='__main__':
-    logging.info("Logging has started.")
+
+
+# if __name__ =='__main__':
+#     logging.info("Logging has started.")
 
